c8a32e1f8fe65391659fe0f903a2e2a9cae1d14e
gavin <gavin@gavinm.com>
2015-08-03 20:10:08 +0100

    working

--- source/components/organization.js
+++ source/components/organization.js
@@ -1,40 +1,47 @@
 var organizationController = {
   list : new signals.Signal(),
-  remove : new signals.Signal(),
+  create : new signals.Signal(),
   save : new signals.Signal(),
   saveComplete : new signals.Signal()
 };
 
-organizationController.remove.add(function(key,id) {
-});
 
-organizationController.list.add(function(key,target) {
-  console.log('organization:list');
-    var url = configuration.scormfu + key + "/organization/all";
-    var html = '';
+organizationController.list.add(function() {
+  var url = configuration.scormfu + _KEY_ + "/organization/all";
+  var html = '';
 
-    $.ajax({
-        url:url, 
-        jsonp: "callback",
-        dataType: "jsonp",
-        success: function( response ) {
-          var source   = $("#organization-list-template").html();
-          var template = Handlebars.compile(source);
-          var html = template(response);
-          $(target).html(html);
-        }
-    });
+  $.ajax({
+    url:url, 
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( response ) {
+      var source   = $("#organization-list-template").html();
+      var template = Handlebars.compile(source);
+      var html = template(response);
+      $('.organization-list').empty().append(html);
+    }
+  });
 
 });
 
-organizationController.save.add(function(key,name,id) {
-  var url = configuration.scormfu + key + "/organization/new/"+name+"/"+id;
+organizationController.create.add(function() {
+  var source   = $("#organization-create-template").html();
+  var template = Handlebars.compile(source);
+  var html = template({});
+  $('.organization-create').empty().append(html).modal();
+});
+
+organizationController.save.add(function() {
+  var name = $('.organization-create #name').val();
+  var id = $('.organization-create #id').val();
+  var url = configuration.scormfu + _KEY_ + "/organization/new/"+name+"/"+id;
   $.ajax({
       url:url, 
       jsonp: "callback",
       dataType: "jsonp",
       success: function( response ) {
-        organizationController.saveComplete.dispatch(response);
+        $('.organization-create').modal('hide');
+        organizationController.list.dispatch();
       }
   });
 });
